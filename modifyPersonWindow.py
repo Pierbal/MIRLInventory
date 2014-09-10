e89b1b46b81915c69422c1f45b0604db648fb78e
glop102 <alexlikeschess@gmail.com>
2014-09-10 08:46:10 -0400

    Have mor on the person selection window, getting clos to adding bindings to opn other windows

--- modifyPersonWindow.py
+++ modifyPersonWindow.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 
-class personWindow:
+class personWindow(Toplevel):
 	def __init__(self,ROOT):
 		self.root=ROOT
+		#llok at selectPersonWindow to see how its done
--- selectPersonWindow.py
+++ selectPersonWindow.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 from modifyPersonWindow import personWindow
+from people import people
 
 class personSelectWindow(Toplevel):
 	def __init__(self,ROOT):
@@ -19,11 +20,32 @@ class personSelectWindow(Toplevel):
 
 		self.wait_window(self)
 
+
 	def body(self):
 		self.elements={}
 		self.elements["personList"]=Listbox(self.rootFrame)
 		self.elements["personList"].grid(row=0,column=0,rowspan=999)
 		self.elements["personList"].focus_set()
+		self.elements["personList"].bind("<Button-1>",self.displayPerson)
+		self.elements["nameLabel"]=Label(self.rootFrame,text="Student Name")
+		self.elements["nameLabel"].grid(row=0,column=1)
+		self.elements["muidLabel"]=Label(self.rootFrame,text="Student Number")
+		self.elements["muidLabel"].grid(row=1,column=1)
+		self.elements["roomLabel"]=Label(self.rootFrame,text="Room")
+		self.elements["roomLabel"].grid(row=2,column=1)
+		self.elements["itemsLabel"]=Label(self.rootFrame,text="Items:\nLine 1\nLine2")
+		self.elements["itemsLabel"].grid(row=3,column=1)
+		
+		self.elements["buttonAddItems"]=Button(self.rootFrame,text="Add Items")
+		self.elements["buttonAddItems"].grid(row=10,column=1)
+
+		self.people=people()
+		for x in self.people.people:
+			print x
+			self.elements["personList"].insert(END,x['name'])
+
+	def displayPerson(self,ignore=""):
+		self.elements["personList"]
 
 	def delete(self,ignore=""):
 		self.grab_release()
